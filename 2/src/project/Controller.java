35208735234f94fda844e2f901c99f00fe06b266
Urepu <darkin31313@gmail.com>
2022-03-24 15:42:35 +0900

    게임 및 점수저장

--- 2/src/project/Controller.java
+++ 2/src/project/Controller.java
@@ -8,7 +8,8 @@ import java.util.regex.Pattern;
 public class Controller {
 	
 	static ArrayList<Member> memberlist = new ArrayList<Member>();
-	Scanner scanner = new Scanner(System.in);
+	static String[] wordList = new String[100];
+ 	Scanner scanner = new Scanner(System.in);
 	// id_valid test
 	public static int id_valid (String id) {
 		// id 정규식  "/^[a-zA-Z0-9]{1,12}$/"  -> 12글자 이하 숫자, 영문
@@ -44,6 +45,54 @@ public class Controller {
 		memberlist.add(member);
 	}
 	
-	
+	//김선제 - gameStart 메소드 작성//
+	public static boolean gameStart(String id, String word) {
+		
+		if(wordList[0] == null) {
+			wordList[0] = word;
+			System.out.println("입력받은 단어: " + word);
+			return true;
+		}else {
+			int index = -1;
+			for(String temp : wordList) {
+				if(temp != null) {
+					index ++;
+				}
+				if(temp != null && temp.equals(word)) {
+					return false;
+				}
+			}
+	        char lastChar = wordList[index].charAt(wordList[index].length() - 1);
+	        char firstChar = word.charAt(0);
+	        if(lastChar == firstChar ) {
+	        	System.out.println("입력받은 단어: " + word);
+	        	wordList[index + 1] = word;
+	        }else {
+	        	return false;
+	        }
+			
+		}
+		return true;
+	}
+	//김선제- 점수 저장
+	public static void save(String id, int index) {
+		for(Member temp : memberlist) {
+			if(temp != null && temp.getId().equals(id)) {
+				if(index > temp.getScore())
+					temp.setScore(index);
+			}
+		}
+		
+	}
+	//김선제- 점수 보기
+
+	public static void seeScore(String id) {
+		for(Member temp : memberlist) {
+			if(temp != null && temp.getId().equals(id)) {
+			System.out.println( temp.getScore());
+
+			}
+		}
+	}
 
 }
